7f3f76e341adf052500ae911726a7b5e2069e66c
Dindamybby <96799064+Dindamybby@users.noreply.github.com>
2022-02-10 15:58:43 +0700

    Update sample_config.py

--- SaitamaRobot/sample_config.py
+++ SaitamaRobot/sample_config.py
@@ -16,8 +16,8 @@ class Config(object):
 
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    TOKEN = "5122084606:AAFYfeAQFhfcDRn3QcG8dcp0Y4YjKWQNSvk"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = purwad1  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Sawada"
     SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
